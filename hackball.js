7e0d7a885f17a848ff041b72bbb5909a6a4fcb16
pi <pi@pi.com>
2015-05-08 13:58:20 +0100

    use 2s delay

--- hackball.js
+++ hackball.js
@@ -1,6 +1,6 @@
 var listener = require('./listener');
 
-var pin22 = listener({ pin: 22 });
+var pin22 = listener({ pin: 22, delay: 2 });
 
 pin22.on('change', function() {
 	console.log('pin 22 changed');
