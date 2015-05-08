2a4837d7f2d13faa83e8b4d15178f00a8fd57493
NiallNiall <niall@nimac.local>
2015-05-08 15:21:18 +0100

    update

--- hackball.js
+++ hackball.js
@@ -1,7 +1,34 @@
-var listener = require('./listener');
+var listener = require('pinhead');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var exec = require('child_process').exec;
 
-var pin22 = listener({ pin: 22, delay: 2 });
+var score = {red: 0 , blue: 0};
 
-pin22.on('change', function() {
-	console.log('pin 22 changed');
+var red = listener({ pin: 22, delay: .5 });
+var blue = listener({ pin: 11, delay: .5 });
+
+red.on('change', function() {
+	goal();
+  score.red++;
 });
+
+blue.on('change', function() {
+  goal();
+  score.blue++;
+});
+
+function goal() {
+  exec('omxplayer static/sound/goaal.mp3');
+  io.emit('score', score);
+}
+
+// give new connections the latest score
+io.on('connection', function (socket) {
+  socket.emit('score', score);
+});
+
+app.use(express.static('static'));
+server.listen(3000);
